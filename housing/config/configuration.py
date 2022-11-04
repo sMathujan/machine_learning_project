cf7c8a2d1d9a2f06c00689bd7f9b034eaf097e8a
sMathujan <sivananthanmathujan@gmail.com>
2022-11-04 08:33:47 +0530

    updated

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -13,12 +13,34 @@ class Configuration:
         config_file_path:str = CONFIG_FILE_PATH,
         current_time_stamp:str = CURRENT_TIME_STAMP
         ) -> None:
-        self.config_info = read_yaml_file(file_path=config_file_path)
-        self.time_stamp = current_time_stamp
-        self.training_pipeline_config = self.get_training_pipeline_config()
+        try:
+            self.config_info = read_yaml_file(file_path=config_file_path)
+            self.time_stamp = current_time_stamp
+            self.training_pipeline_config = self.get_training_pipeline_config()
+        except Exception as e:
+            raise HousingException(e, sys) from e
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
-        pass
+        try:
+            self.config_info
+
+            dataset_download_url
+            tgz_download_dir
+            raw_data_dir
+            ingested_train_dir
+            ingested_test_dir
+            data_ingestion_config = DataIngestionConfig(
+                dataset_download_url=dataset_download_url,
+                tgz_download_dir=tgz_download_dir,
+                raw_data_dir=raw_data_dir,
+                ingested_train_dir=ingested_train_dir,
+                ingested_test_dir=ingested_test_dir
+            )
+            logging.info(f"Data Ingestion config: {data_ingestion_config}")
+            return data_ingestion_config
+
+        except Exception as e:
+            raise HousingException(e, sys) from e
 
     def get_data_validation_config(self) -> DataValidationConfig:
         pass
